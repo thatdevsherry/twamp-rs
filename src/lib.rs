40884382a8f767da206e22ddbe8cb45243731bf6
Shehriyar Qureshi <thatdevsherry@gmail.com>
2025-11-02 21:09:15 +0500

    chore: add some docs for hyperlink fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+//! TWAMP Protocol Implementation [RFC 5357](https://datatracker.ietf.org/doc/rfc5357)
+//!
+//! This crate provides a complete implementation of the TWAMP protocol,
+//! organized into the following components:
+//!
+//! - [`control_client`] - Control client implementation
+//! - [`server`] - Server implementation  
+//! - [`session_reflector`] - Session reflector
+//! - [`session_sender`] - Session sender
+//! - [`timestamp`] - Timestamp utilities
+//! - [`twamp_control`] - TWAMP control protocol
+//! - [`twamp_test`] - TWAMP test protocol
+
 pub use control_client;
 pub use server;
 pub use session_reflector;

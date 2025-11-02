98ddacbda07512912d9189d81578ad6e69cef8f5
Shehriyar Qureshi <thatdevsherry@gmail.com>
2025-11-02 23:02:53 +0500

    chore(.github)!: try inlining re-exports
    no idea what it does. testing for now

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
+#[doc(inline)]
 pub use control_client;
+#[doc(inline)]
 pub use server;
+#[doc(inline)]
 pub use session_reflector;
+#[doc(inline)]
 pub use session_sender;
+#[doc(inline)]
 pub use timestamp;
+#[doc(inline)]
 pub use twamp_control;
+#[doc(inline)]
 pub use twamp_test;

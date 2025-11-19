dfba82d0adb5cb28680564139f7251a9349355a1
Shehriyar Qureshi <thatdevsherry@gmail.com>
2025-11-19 18:03:31 +0500

    feat(control_client): add custom errors

--- src/control_client/mod.rs
+++ src/control_client/mod.rs
@@ -1,5 +1,6 @@
 use crate::twamp_control::Accept;
 use crate::twamp_control::AcceptSession;
+use crate::twamp_control::ControlMessage;
 use crate::twamp_control::RequestTwSession;
 use crate::twamp_control::SecurityMode;
 use crate::twamp_control::ServerGreeting;
@@ -10,11 +11,31 @@ use crate::twamp_control::StartSessions;
 use crate::twamp_control::StopSessions;
 use anyhow::{Result, anyhow};
 use deku::prelude::*;
+use std::io;
 use std::net::IpAddr;
+use thiserror::Error;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 use tokio::sync::oneshot;
 use tracing::*;
+
+#[derive(Error, Debug)]
+pub enum ControlClientError {
+    #[error("failed to read from stream")]
+    ReadError(ControlMessage, #[source] io::Error),
+
+    #[error("failed to write to stream")]
+    WriteError(ControlMessage, #[source] io::Error),
+
+    #[error("failed to convert b/w rust and wire format.")]
+    WireConversionError(#[source] DekuError),
+
+    #[error("Control-Client is not bound to TcpStream")]
+    NotBound(#[source] io::Error),
+
+    #[error("Server was not connected to TcpStream")]
+    ServerNotConnected(#[source] io::Error),
+}
 
 /// Control-Client is responsible for initiating and handling TWAMP-Control with a
 /// [Server](crate::server::Server).
@@ -62,13 +83,17 @@ impl ControlClient {
         };
 
         debug!("Responder provided port: {}", accept_session.port);
-        reflector_port_tx.send(accept_session.port).unwrap();
+        reflector_port_tx
+            .send(accept_session.port)
+            .expect("reflector port should be sent over channel.");
         self.send_start_sessions().await?;
         let start_ack = self.read_start_ack().await?;
         if start_ack.accept != Accept::Ok {
             return Err(anyhow!("Start-Ack should be zero"));
         }
-        start_session_tx.send(()).unwrap();
+        start_session_tx
+            .send(())
+            .expect("StartSession signal should be sent over channel");
         // testing
         debug!(
             "Waiting for Session-Sender to complete, Control-Client will then send Stop-Sessions."
@@ -83,7 +108,12 @@ impl ControlClient {
     pub async fn read_server_greeting(&mut self) -> Result<ServerGreeting> {
         let mut buf = [0; ServerGreeting::SERIALIZED_SIZE];
         info!("Reading ServerGreeting");
-        self.stream.as_mut().unwrap().read_exact(&mut buf).await?;
+        self.stream
+            .as_mut()
+            .expect("TcpStream should be present")
+            .read_exact(&mut buf)
+            .await
+            .map_err(|err| ControlClientError::ReadError(ControlMessage::ServerGreeting, err))?;
         let (_rest, server_greeting) = ServerGreeting::from_bytes((&buf, 0))
             .expect("should have received valid ServerGreeting");
         debug!("Server greeting: {:?}", server_greeting);
@@ -94,14 +124,19 @@ impl ControlClient {
     /// Creates a `SetUpResponse`, converts to bytes and sends it out on `TWAMP-Control`.
     pub async fn send_set_up_response(&mut self) -> Result<()> {
         info!("Preparing to send Set-Up-Response");
-        let set_up_response = SetUpResponse::new(SecurityMode::Unauthenticated);
+        let security_mode = SecurityMode::Unauthenticated;
+        let set_up_response = SetUpResponse::new(security_mode);
         debug!("Set-Up-Response: {:?}", set_up_response);
-        let encoded = set_up_response.unwrap().to_bytes().unwrap();
+        let encoded = set_up_response
+            .expect("SetUpResponse should be created")
+            .to_bytes()
+            .map_err(|err| ControlClientError::WireConversionError(err))?;
         self.stream
             .as_mut()
-            .unwrap()
+            .expect("TcpStream should be present")
             .write_all(&encoded[..])
-            .await?;
+            .await
+            .map_err(|err| ControlClientError::WriteError(ControlMessage::SetUpResponse, err))?;
         info!("Set-Up-Response sent");
         Ok(())
     }
@@ -111,8 +146,14 @@ impl ControlClient {
     pub async fn read_server_start(&mut self) -> Result<ServerStart> {
         let mut buf = [0; ServerStart::SERIALIZED_SIZE];
         info!("Reading Server-Start");
-        self.stream.as_mut().unwrap().read_exact(&mut buf).await?;
-        let (_rest, server_start) = ServerStart::from_bytes((&buf, 0)).unwrap();
+        self.stream
+            .as_mut()
+            .expect("TcpStream should be present")
+            .read_exact(&mut buf)
+            .await
+            .map_err(|err| ControlClientError::ReadError(ControlMessage::ServerStart, err))?;
+        let (_rest, server_start) = ServerStart::from_bytes((&buf, 0))
+            .map_err(|err| ControlClientError::WireConversionError(err))?;
         debug!("Server-Start: {:?}", server_start);
         info!("Done reading Server-Start");
         Ok(server_start)
@@ -126,12 +167,20 @@ impl ControlClient {
         timeout: u64,
     ) -> Result<RequestTwSession> {
         info!("Preparing to send Request-TW-Session");
-        let stream = self.stream.as_ref().unwrap();
-        let sender_address = match stream.local_addr().unwrap().ip() {
+        let stream = self.stream.as_ref().expect("TcpStream should be present");
+        let sender_address = match stream
+            .local_addr()
+            .map_err(|err| ControlClientError::NotBound(err))?
+            .ip()
+        {
             IpAddr::V4(ip) => ip,
             IpAddr::V6(ip) => panic!("da hail did v6 come from: {ip}"),
         };
-        let receiver_address = match stream.peer_addr().unwrap().ip() {
+        let receiver_address = match stream
+            .peer_addr()
+            .map_err(|err| ControlClientError::ServerNotConnected(err))?
+            .ip()
+        {
             IpAddr::V4(ip) => ip,
             IpAddr::V6(ip) => panic!("da hail did v6 come from: {ip}"),
         };
@@ -148,10 +197,12 @@ impl ControlClient {
             timeout,
         );
         debug!("request-tw-session: {:?}", request_tw_session);
-        let encoded = request_tw_session.to_bytes().unwrap();
+        let encoded = request_tw_session
+            .to_bytes()
+            .map_err(|err| ControlClientError::WireConversionError(err))?;
         self.stream
             .as_mut()
-            .unwrap()
+            .expect("TcpStream should be present")
             .write_all(&encoded[..])
             .await?;
         info!("Request-TW-Session sent");
@@ -163,8 +214,13 @@ impl ControlClient {
     pub async fn read_accept_session(&mut self) -> Result<AcceptSession> {
         let mut buf = [0; AcceptSession::SERIALIZED_SIZE];
         info!("Reading Accept-Session");
-        self.stream.as_mut().unwrap().read_exact(&mut buf).await?;
-        let (_rest, accept_session) = AcceptSession::from_bytes((&buf, 0)).unwrap();
+        self.stream
+            .as_mut()
+            .expect("TcpStream should be present")
+            .read_exact(&mut buf)
+            .await?;
+        let (_rest, accept_session) = AcceptSession::from_bytes((&buf, 0))
+            .map_err(|err| ControlClientError::WireConversionError(err))?;
         debug!("Accept-Session: {:?}", accept_session);
         info!("Read Accept-Session");
 
@@ -176,10 +232,12 @@ impl ControlClient {
         info!("Preparing to send Start-Sessions");
         let start_sessions = StartSessions::new();
         debug!("Start-Sessions: {:?}", start_sessions);
-        let encoded = start_sessions.to_bytes().unwrap();
+        let encoded = start_sessions
+            .to_bytes()
+            .map_err(|err| ControlClientError::WireConversionError(err))?;
         self.stream
             .as_mut()
-            .unwrap()
+            .expect("TcpStream should be present")
             .write_all(&encoded[..])
             .await?;
         info!("Start-Sessions sent");
@@ -191,8 +249,13 @@ impl ControlClient {
     pub async fn read_start_ack(&mut self) -> Result<StartAck> {
         let mut buf = [0; StartAck::SERIALIZED_SIZE];
         info!("Reading Start-Ack");
-        self.stream.as_mut().unwrap().read_exact(&mut buf).await?;
-        let (_rest, start_ack) = StartAck::from_bytes((&buf, 0)).unwrap();
+        self.stream
+            .as_mut()
+            .expect("TcpStream should be present")
+            .read_exact(&mut buf)
+            .await?;
+        let (_rest, start_ack) = StartAck::from_bytes((&buf, 0))
+            .map_err(|err| ControlClientError::WireConversionError(err))?;
         debug!("Start-Ack: {:?}", start_ack);
         info!("Done reading Start-Ack");
         Ok(start_ack)
@@ -203,10 +266,12 @@ impl ControlClient {
         info!("Preparing to send Stop-Sessions");
         let stop_sessions = StopSessions::new(Accept::Ok);
         debug!("Stop-Sessions: {:?}", stop_sessions);
-        let encoded = stop_sessions.to_bytes().unwrap();
+        let encoded = stop_sessions
+            .to_bytes()
+            .map_err(|err| ControlClientError::WireConversionError(err))?;
         self.stream
             .as_mut()
-            .unwrap()
+            .expect("TcpStream should be present")
             .write_all(&encoded[..])
             .await?;
         info!("Stop-Sessions sent");
